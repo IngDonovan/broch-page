89052051fb9fa498b9430d063f640cab7ec5e8fe
Donovan Rojas <rojasdrm03@gmail.com>
2023-09-18 20:27:13 -0500

    footer component

---
+++ src/Components/InfoFooter/index.jsx
@@ -0,0 +1,25 @@
+
+// import { TiThMenuOutline } from "react-icons/ti"
+
+import "./InfoFooter.scss";
+
+const InfoFooter = () => {
+
+
+  return (
+    <footer>
+        <ul>
+            <li>D</li>
+            <li>D'loil SAS</li>
+            <li>Sobre Nosotros</li>
+            <li>Contacto</li>
+        </ul>
+        <span>
+            <p>Design by</p>
+            <span>Ing DRM</span>
+        </span>
+    </footer>
+  );
+};
+
+export default InfoFooter;
