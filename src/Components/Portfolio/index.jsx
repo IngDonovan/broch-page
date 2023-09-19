c9a32f5d91ad42fb4d7ccc1def2bd4e317366f7a
Donovan Rojas <rojasdrm03@gmail.com>
2023-09-19 17:43:03 -0500

    styles first section about portfolio

--- src/Components/Portfolio/index.jsx
+++ src/Components/Portfolio/index.jsx
@@ -12,7 +12,7 @@ const Portfolio = () => {
                 <h1>Ingeniera de petroleos & Auditora de Gestión</h1>
             </div>
             <picture>
-                <img src="" alt="" />
+                <img src="https://s3-alpha-sig.figma.com/img/f682/07c2/7aadffad2c21abeaadb6613d852ae7b6?Expires=1696204800&Signature=GNCaiCtzv7MYN7wWHhSa4WZK9ghL30kEpQqPL4eULzk0aA8kZiJmdOXGZ0DQaP03Bfu6TCxlamrowZLai6JagRQIedbPJwhS0zNDddTYDRgcalr80T0KIxQuVovtE1cgyvmJNM4V1U0YQ4ZaWPKy1V~qH1L4OqGRKxq~~bkEB3pW7FF6RFI7pe11qu8cpdylI49tpIbZ55Fds23ILahFYo3yJkyNHbDybjiADELX4QBaWwz536yS2C7iakyAgeSC8CxLoUF6iVFuJkmrNBhtV~tT5uKbZforICPqXif0pybpcip3c4VeVrBCoZZ508lpMsXz13PWL5J~yAWxxBmgXw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
             </picture>
         </article>
         <aside>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -4,7 +4,9 @@ function Home() {
 
   return (
     <>
+      <main>
         <Portfolio />
+      </main>
     </>
   )
 }
