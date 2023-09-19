ac7c03435c1301af98c7c5f5cf8db944fe73fa03
Donovan Rojas <rojasdrm03@gmail.com>
2023-09-19 17:00:41 -0500

    Portfolio Component

---
+++ src/Components/Portfolio/index.jsx
@@ -0,0 +1,26 @@
+// import { TiThMenuOutline } from "react-icons/ti"
+
+import "./Portfolio.scss";
+
+const Portfolio = () => {
+
+  return (
+    <section>
+        <article>
+            <div>
+                <p>Hola, yo soy</p>
+                <h1>Ingeniera de petroleos & Auditora de Gestión</h1>
+            </div>
+            <picture>
+                <img src="" alt="" />
+            </picture>
+        </article>
+        <aside>
+            <h3>Servicios</h3>
+            card
+        </aside>
+    </section>
+  );
+};
+
+export default Portfolio;
