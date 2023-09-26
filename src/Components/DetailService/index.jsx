5863f99d481e77d3d50679028163015a6dab403c
Donovan Rojas <rojasdrm03@gmail.com>
2023-09-26 15:20:07 -0500

    DetailService create

---
+++ src/Components/DetailService/index.jsx
@@ -0,0 +1,18 @@
+import "./DetailService.scss";
+
+const DetailService = () => {
+
+
+  return (
+    <section>
+        <span>
+            <h2>title</h2>
+            <ul>
+                <li>lista</li>
+            </ul>
+        </span>
+    </section>
+  );
+};
+
+export default DetailService;
