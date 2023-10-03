cf882c25187926fe5820d117179824a34500a925
Donovan Rojas <rojasdrm03@gmail.com>
2023-10-03 15:57:22 -0500

    low fix

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -10,7 +10,7 @@ const EdnaProvider = ({ children }) => {
     {
       id: 1,
       title:'Implementación de Sistemas de Gestión',
-      url:'https://s3-alpha-sig.figma.com/img/bfc2/f4b7/72a48e19f9ed9ea46873e23e685fafbf?Expires=1696204800&Signature=hsV2OtAJmb0KYUZd7AIN-DzllYQbu8DLeKgZE8aBsjUkGDFjX1OL56H-2FfOyXEbkaCTeoVblifoLgyyZyjKcTYnKXpEUealKoxqQubvQA3l2pwIPjvyfvCuQarruU4u8jFKXJdfoBjGOgLt7zrLZ7o2C6u1kgR~wQQZMyU5RYUx~VM~T~n8JP7R2u~wQRV0xPoiVukw7jig5qJn8y7qOHDYbvrrdjs8xAMRxynHoL6LyYYHv3OjoOCiq~3p97vhtRbpURUPCQPRQ~qUFNgiYkQornCHEoI28ShccGVMTcLhjzHjA-pU7dn1DtkstZSNE-bEtAgu3xoYp~sS7UtiUw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+      url:'https://images.pexels.com/photos/8297478/pexels-photo-8297478.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
       detail:[
         "BAJO NORMAS:",
         "SG- SST (Dec. 1072/2015. Libro 2, Parte 2 ,Titulo 4, capitulo 6. / Res 0312/2019).",
@@ -25,7 +25,7 @@ const EdnaProvider = ({ children }) => {
     {
       id: 2,
       title:'Auditorias Internas y Diagnósticos',
-      url:'https://s3-alpha-sig.figma.com/img/8fd5/0908/eeed7a92b5934824ec8ea533ebcc68a3?Expires=1696204800&Signature=KEfCs9y-Rv0mEqcgiXb6ojOZqs4fgt4Wo0OMF7KCVGWRrcT16mJizIYe03ra~XIqtpVfM~F1zHG4le0wrV1kRJMc7VnaanvC9aV8AFIp6ogF~98rH4H-5QmLvFeluy4fdxGlLWiaOXlDlfClkMQ3b-OTvaeOjCrM1nZmR8O0lIgnguWdk~C3qYGHTGe26ex3-e2dzjdaYwbvpS1U-pwp4SOeQsbuJfS9r8BfAeWHpNt6lv0dU~KCFdgUEVKRFwfW5TzIKEssdb5OOn1yQF1RFyFlN97fcp6eAEeaMBuZItAoyL5PaWq98RM7MJHVBltL--ienX~3ZTqyDEpWI-0Dpw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+      url:'https://images.pexels.com/photos/8190804/pexels-photo-8190804.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
       detail:[
         "BAJO NORMAS:",
         "SG- SST (Dec. 1072/2015. Libro 2, Parte 2, Titulo 4, capitulo 6. / Res 0312/2019).",
@@ -41,7 +41,7 @@ const EdnaProvider = ({ children }) => {
     {
       id: 3,
       title:'Aseguramiento Documental',
-      url:'https://s3-alpha-sig.figma.com/img/9704/4ed7/ea7838bf084b27e95b9f540c1ed49801?Expires=1696204800&Signature=HApzipg7pVowsVshoUJlYQflDqM-wLB6ITF6jYvglkp-~3Z9y~0IYG6aa-wZRIuZFlsQLJbcaa94F4nTVZ251NMfdIqXhdWNpvg-gsWnAgbY7P6q0Us11EpVLU5haq-vXUQWgOAZxISbuWdJOgsLQoziV--ysacYVHyCgRvSuzegEL9LnuhDX~ke-G8MMjknkasz3-dPzCAZ5aRG56UMp7QCjSU8FRjkoGGOowiS0R4znXK6r9upiHOg54JqNqSPSGYT1qGza3QSt1k5ZuceZzaSbdLrblK09mjXoxB~YOSty6821rQmpOkGQbolbGjMkLGsdoK9VyLkWEz7gVu5TQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+      url:'https://images.pexels.com/photos/8297445/pexels-photo-8297445.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
       detail:[
         "Elaboración del PESV.",
         "Elaboración de procedimientos internos",
@@ -52,7 +52,7 @@ const EdnaProvider = ({ children }) => {
     {
       id: 4,
       title:'Capacitaciones y Otras Actividades',
-      url:'https://s3-alpha-sig.figma.com/img/7afd/0c31/0280a609e647d4044696a3634b19091e?Expires=1696204800&Signature=JCOBTmM-AdybPJP5S1Cu8EcTizjUIRtkc0TEx5TPiBHTvSlg7T7T5cw8H~XCr0zOlZ-qCWfyp7s56zqWA2QAYvsJkelmY49-Kx6BGii~f~Rk8wrqq7DkGa0EWdOJJSnxcy7fv4cfkXTkuXQSDqriYXIEFkiYOY4m4eAj6OEF0UU5hHuQ0mW6ngt1avM2chHOqHoMuE~eeHSRPnN3ziay6Ua5aBZX25FI6eda3Cd9D5QJr2Tud2OMjzSy2Scvx6KmVOtEPrGxB7baY50Lh7yHvoBTrH8gmrnmUvnZczzwvO5Df4vagKKiiZqmx4BzQQKpOpWeVXLGYneIfNiWa3VhDw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+      url:'https://images.pexels.com/photos/7648468/pexels-photo-7648468.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
       detail:[
         "Capacitaciones relacionadas con : SST, Calidad, Ambiental y otras normas.",
         "Autoevaluación del sistema bajo SST. (Res. 0312/2019).",
@@ -65,7 +65,7 @@ const EdnaProvider = ({ children }) => {
     {
       id: 5,
       title:'Otros',
-      url:'https://s3-alpha-sig.figma.com/img/bfc2/f4b7/72a48e19f9ed9ea46873e23e685fafbf?Expires=1696204800&Signature=hsV2OtAJmb0KYUZd7AIN-DzllYQbu8DLeKgZE8aBsjUkGDFjX1OL56H-2FfOyXEbkaCTeoVblifoLgyyZyjKcTYnKXpEUealKoxqQubvQA3l2pwIPjvyfvCuQarruU4u8jFKXJdfoBjGOgLt7zrLZ7o2C6u1kgR~wQQZMyU5RYUx~VM~T~n8JP7R2u~wQRV0xPoiVukw7jig5qJn8y7qOHDYbvrrdjs8xAMRxynHoL6LyYYHv3OjoOCiq~3p97vhtRbpURUPCQPRQ~qUFNgiYkQornCHEoI28ShccGVMTcLhjzHjA-pU7dn1DtkstZSNE-bEtAgu3xoYp~sS7UtiUw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+      url:'https://images.pexels.com/photos/7438102/pexels-photo-7438102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     },
   ];
 
